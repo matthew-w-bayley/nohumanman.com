a134c39e08d1519de67b7345680549f73da03569
nohumanman <nohumanman@gmail.com>
2023-05-08 16:49:50 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+// because we're not using jquery
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+
+document.getElementById("signature").innerText = httpGet("signature.txt");
+
