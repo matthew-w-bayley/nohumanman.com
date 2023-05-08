6047277cd768835a3e2ba90ec78d748d77f55fdf
nohumanman <nohumanman@gmail.com>
2023-05-08 17:35:43 +0100

    modified for aesthetics and added socials

--- main.js
+++ main.js
@@ -10,3 +10,20 @@ function httpGet(theUrl)
 
 document.getElementById("signature").innerText = httpGet("signature.txt");
 
+socials = [
+    {
+        "logo": "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        "src": "https://github.com/nohumanman"
+    },
+    {
+        "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png",
+        "src": "https://www.linkedin.com/in/matthew-w-bayley/"
+    }
+]
+
+socials.forEach(element => {
+    console.log(element);
+    logo = element["logo"];
+    src = element["src"];
+    document.getElementById("socials").innerHTML += "<a target='_blank' href='" + src + "'><img style='border-radius:5px' height='50' src='" + logo + "'/></a>&nbsp;&nbsp;";
+});
